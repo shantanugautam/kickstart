819fde3574aaee7fe4913cb14e59055db6459677
Shantanu Gautam <shantanu@matchmove.com>
2015-02-11 00:13:27 +0800

    Login Module Added

--- src/components/feedback/feedback.controller.js
+++ src/components/feedback/feedback.controller.js
@@ -2,15 +2,11 @@
 
 angular.module('mmLandingPages')
   .controller('feedbackCtrl', function ($scope, $firebase) {
-    // $scope.date = new Date();
     var ref = new Firebase('https://resplendent-fire-5403.firebaseio.com/');
     $scope.users = $firebase(ref).$asArray();
-    console.log($scope.users);
     $scope.feedbackSuccess = false;
     $scope.user = {};
-    console.log($scope.user);
     $scope.addUser = function() {
-        console.log($scope.user);
         $scope.users.$add($scope.user);
         $scope.feedbackSuccess = true;
     }
--- src/components/registerinterest/registerinterest.controller.js
+++ src/components/registerinterest/registerinterest.controller.js
@@ -4,15 +4,9 @@ angular.module('mmLandingPages')
   .controller('registerinterestCtrl', function ($scope, $firebase) {
     var ref = new Firebase('https://resplendent-fire-5403.firebaseio.com/');
     $scope.users = $firebase(ref).$asArray();
-
-    // $scope.date = new Date();
-    console.log($scope.users);
     $scope.registerSuccess = false;
-    // $scope.date = new Date();
     $scope.user = {};
-    console.log($scope.user);
     $scope.addUser = function() {
-        console.log($scope.user);
         $scope.users.$add($scope.user);
         $scope.registerSuccess = true;
     }
