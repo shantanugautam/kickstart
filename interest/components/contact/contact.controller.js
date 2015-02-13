b72b420fdb3621d20a01a12a3ddb3befabe63d97
Shantanu Gautam <shantanu@matchmove.com>
2015-02-13 09:14:21 +0800

    UPdate the Build process with contact

---
+++ interest/components/contact/contact.controller.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+angular.module('mmLandingPages')
+  .controller('contactCtrl', function ($scope, $firebase, $location, $timeout, ngDialog) {
+    var base = 'interest';
+    var url = 'https://resplendent-fire-5403.firebaseio.com/' + base + '/correspondence';
+    var ref = new Firebase(url);
+    $scope.requests = $firebase(ref).$asArray();
+    $scope.requestSuccess = false;
+    $scope.request = {};
+    $scope.addRequest = function() {
+        $scope.request.useragent = navigator.userAgent;
+        $scope.request.referrer = document.referrer;
+        $scope.request.domain = window.location.hostname;
+        $scope.request.timestamp = Firebase.ServerValue.TIMESTAMP;
+        $scope.requests.$add($scope.request);
+        $scope.requestSuccess = true;
+        $timeout( function(){ ngDialog.close(); }, 3000);
+    }
+  });
