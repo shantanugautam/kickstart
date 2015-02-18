62ee2e0a957b9f662e8349212afe31fdc8433089
Shantanu Gautam <shantanu@matchmove.com>
2015-02-18 15:26:54 +0800

    Update

--- src/app/index.js
+++ src/app/index.js
@@ -16,16 +16,6 @@ angular.module('mmLandingPages',
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
-      })
-      .state('login', {
-        url: '/login',
-        templateUrl: 'app/login/login.html',
-        controller: 'LoginCtrl'
-      })
-      .state('admin', {
-        url: '/admin',
-        templateUrl: 'app/admin/admin.html',
-        controller: 'AdminCtrl'
       });
     $urlRouterProvider.otherwise('/');
   })
